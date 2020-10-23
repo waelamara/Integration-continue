9d0ffb947bb4d7fbe741723dadcb093f9c112178
Oussama Dababi <oussama.dababi@esprit.tn>
2020-10-23 17:01:24 +0100

    DepartementServiceImp + DepartementServiceImpTest ContratServiceImp +ContratServiceImpTest

---
+++ src/test/java/tn/esprit/spring/ContratServiceImplTest.java
@@ -0,0 +1,27 @@
+package tn.esprit.spring;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import tn.esprit.spring.services.ContratServiceImpl;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContratServiceImplTest {
+	@Autowired
+	ContratServiceImpl CSI;
+	
+	@Test
+	public void getAllContrats(){
+		assertEquals(1,CSI.getAllContrats().size());
+		
+		
+	}
+	
+
+}
